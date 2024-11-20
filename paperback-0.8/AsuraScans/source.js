4e486906f151a3d1acb0cfaf25c67bf92e920eed
Ivanmatthew <76107136+Ivanmatthew@users.noreply.github.com>
2024-11-20 03:57:31 +0000

    Deploying to gh-pages from @ Ivanmatthew/ivans-paperback-extensions@cab704954562a3459d42c19f519a36d48818310d 🚀

--- paperback-0.8/AsuraScans/source.js
+++ paperback-0.8/AsuraScans/source.js
@@ -14918,7 +14918,7 @@ var _Sources = (() => {
     const textBufferRepr = parseNextJSData($3);
     textBufferRepr.finalize();
     const mangaDetailsObject = textBufferRepr.resolveIndexWithHex(
-      "3b",
+      "4b",
       (inp) => recurseParseJSON(inp)
     );
     const title = mangaDetailsObject.name ?? "";
