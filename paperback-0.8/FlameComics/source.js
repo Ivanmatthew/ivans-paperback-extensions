435afeacf908f9ae58c9eb2819fbf5a1ad831b2f
Ivanmatthew <76107136+Ivanmatthew@users.noreply.github.com>
2025-02-23 01:14:00 +0000

    Deploying to gh-pages from @ Ivanmatthew/ivans-paperback-extensions@5cf1071f9c59c3079bed9eb7fc7eae842a78a4e6 🚀

--- paperback-0.8/FlameComics/source.js
+++ paperback-0.8/FlameComics/source.js
@@ -14598,7 +14598,7 @@ var _Sources = (() => {
   var FLAMECOMICS_DOMAIN = "https://flamecomics.xyz";
   var FLAMECOMICS_CDN_DOMAIN = "https://cdn.flamecomics.xyz";
   var FlameComicsInfo = {
-    version: "1.1.2",
+    version: "1.1.3",
     name: "FlameComics",
     description: "Flame comics source for 0.8",
     author: "IvanMatthew",
@@ -14896,7 +14896,7 @@ var _Sources = (() => {
             const includedTags = query.includedTags.map(
               (tag) => tag.label
             );
-            const comicTags = comic.tags;
+            const comicTags = comic.categories;
             return includedTags.some(
               (tag) => comicTags.includes(tag)
             );
@@ -14907,7 +14907,7 @@ var _Sources = (() => {
             const excludedTags = query.excludedTags.map(
               (tag) => tag.label
             );
-            const comicTags = comic.tags;
+            const comicTags = comic.categories;
             return !excludedTags.some(
               (tag) => comicTags.includes(tag)
             );
